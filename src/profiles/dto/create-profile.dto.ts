b3557420718828cb856f61b1f8110448f3af0970
石渡拓海 <t-ishiwata@sight-visit.com>
2021-11-20 22:16:05 +0900

    [Add] Mutationの追加

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,12 +1,17 @@
 import { IsString } from 'class-validator';
+import { Field, InputType, Int } from '@nestjs/graphql';
 
+@InputType()
 export class CreateProfileDto {
+  @Field()
   @IsString()
   githubUrl: string;
 
+  @Field()
   @IsString()
   twitterId: string;
 
+  @Field()
   @IsString()
   description: string;
 }
--- src/profiles/profiles.resolver.ts
+++ src/profiles/profiles.resolver.ts
@@ -1,13 +1,19 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver, Mutation } from '@nestjs/graphql';
+import { CreateProfileDto } from './dto/create-profile.dto';
 import { Profile } from './entities/profile.entity';
 import { ProfilesService } from './profiles.service';
 
-@Resolver((of) => Profile)
+@Resolver(() => Profile)
 export class ProfilesResolver {
   constructor(private profilesService: ProfilesService) {}
 
-  @Query((returns) => Profile)
+  @Query(() => Profile)
   async profile(@Args('id', { type: () => Int }) id: number) {
     return this.profilesService.findOne(id);
+  }
+
+  @Mutation(() => Profile)
+  async createProfile(@Args('newProfile') newProfile: CreateProfileDto) {
+    return this.profilesService.create(newProfile);
   }
 }
