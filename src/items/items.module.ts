fecbeea434555cb53aa3fdb8712231a5d9dcc50d
石渡拓海 <t-ishiwata@sight-visit.com>
2021-11-21 23:10:55 +0900

    [Add] ItemsModuleの追加

---
+++ src/items/items.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DateScalar } from 'src/common/scalars/date.scalar';
+import { Item } from './entities/item.entity';
+import { ItemsResolver } from './items.resolver';
+import { ItemsService } from './items.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Item])],
+  providers: [ItemsResolver, ItemsService, DateScalar],
+})
+export class ItemsModule {}
