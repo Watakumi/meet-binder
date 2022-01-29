37dcb191dc4cbcea47950ec606604f6254e03f86
石渡拓海 <t-ishiwata@sight-visit.com>
2022-01-29 16:31:49 +0900

    items.serviceのspecの修正

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,7 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ProfileItem } from '../profile-items/entities/profile-item.entity';
+import { Connection } from 'typeorm';
+import { Item } from './entities/item.entity';
 import { ItemsService } from './items.service';
 
 describe('ItemsService', () => {
+  let service: ItemsService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ItemsService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(Item), useValue: {} },
+        { provide: getRepositoryToken(ProfileItem), useValue: {} },
+        { provide: getRepositoryToken(Item), useValue: {} },
+      ],
+    }).compile();
+
+    service = module.get<ItemsService>(ItemsService);
+  });
   it('should be defined', () => {
-    expect(new ItemsService()).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
