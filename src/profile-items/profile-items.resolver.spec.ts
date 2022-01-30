24ea07316810099c88ac8f68f20e57048b71a0ba
石渡拓海 <t-ishiwata@sight-visit.com>
2022-01-31 00:19:01 +0900

    テストを通るようにする

--- src/profile-items/profile-items.resolver.spec.ts
+++ src/profile-items/profile-items.resolver.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileItemsResolver } from './profile-items.resolver';
+import { ProfileItemsService } from './profile-items.service';
 
 describe('ProfileItemsResolver', () => {
   let resolver: ProfileItemsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfileItemsResolver],
+      providers: [
+        ProfileItemsResolver,
+        {
+          provide: ProfileItemsService,
+          useValue: () => ({ findAll: jest.fn(), create: jest.fn() }),
+        },
+      ],
     }).compile();
 
     resolver = module.get<ProfileItemsResolver>(ProfileItemsResolver);
