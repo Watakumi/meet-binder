f8b90be341a30bfbe1b8cc183a2a896a154bdbe2
石渡拓海 <t-ishiwata@sight-visit.com>
2022-01-30 23:05:36 +0900

    resolverの通るテストを書く

--- src/items/items.resolver.spec.ts
+++ src/items/items.resolver.spec.ts
@@ -1,7 +1,41 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsResolver } from './items.resolver';
+import { ItemsService } from './items.service';
 
 describe('ItemsResolver', () => {
-  it('should be defined', () => {
-    expect(new ItemsResolver()).toBeDefined();
+  let resolver: ItemsResolver;
+  const item = {
+    id: 1,
+    name: 'item',
+    createdAt: '2022-01-01 12:00:00',
+    updatedAt: '2022-01-01 12:00:00',
+  };
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        ItemsResolver,
+        {
+          provide: ItemsService,
+          useValue: () => ({
+            findAll: jest.fn(() => [item]),
+            findOne: jest.fn(),
+            create: jest.fn(),
+          }),
+        },
+      ],
+    }).compile();
+
+    resolver = moduleRef.get<ItemsResolver>(ItemsResolver);
   });
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+
+  // describe('items', () => {
+  //   // it('should return an array of items', async () => {
+  //   //   const result = [];
+  //   //   jest.spyOn(itemsService, 'findAll').mockImplementation(() => result);
+  //   // });
+  // });
 });
