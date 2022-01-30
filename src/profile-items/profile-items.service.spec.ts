7a0fa5a013bb5f9a01faeb96fd1da0fdddc0cc41
石渡拓海 <t-ishiwata@sight-visit.com>
2022-01-31 00:44:21 +0900

    specの追加

--- src/profile-items/profile-items.service.spec.ts
+++ src/profile-items/profile-items.service.spec.ts
@@ -1,7 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from '../items/entities/item.entity';
+import { Profile } from '../profiles/entities/profile.entity';
+import { Connection } from 'typeorm';
+import { ProfileItem } from './entities/profile-item.entity';
 import { ProfileItemsService } from './profile-items.service';
 
 describe('ProfileItemsService', () => {
+  let service: ProfileItemsService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ProfileItemsService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(ProfileItem), useValue: {} },
+        { provide: getRepositoryToken(Item), useValue: {} },
+        { provide: getRepositoryToken(Profile), useValue: {} },
+      ],
+    }).compile();
+
+    service = module.get<ProfileItemsService>(ProfileItemsService);
+  });
   it('should be defined', () => {
-    expect(new ProfileItemsService()).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
