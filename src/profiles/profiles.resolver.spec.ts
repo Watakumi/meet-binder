58dac31d383c6f605e6437da4e71aa62df92523e
石渡拓海 <t-ishiwata@sight-visit.com>
2022-01-31 00:44:30 +0900

    specの追加

--- src/profiles/profiles.resolver.spec.ts
+++ src/profiles/profiles.resolver.spec.ts
@@ -1,7 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesResolver } from './profiles.resolver';
+import { ProfilesService } from './profiles.service';
 
 describe('ProfilesResolver', () => {
+  let resolver: ProfilesResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ProfilesResolver,
+        {
+          provide: ProfilesService,
+          useValue: () => ({
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+          }),
+        },
+      ],
+    }).compile();
+
+    resolver = module.get<ProfilesResolver>(ProfilesResolver);
+  });
+
   it('should be defined', () => {
-    expect(new ProfilesResolver()).toBeDefined();
+    expect(resolver).toBeDefined();
   });
 });
